d62fade1882021b3e60bf20eae2922e9c4d0752b
andres-perez <apd.pro@gmail.com>
2013-07-23 04:58:45 +0000

    index.html added and web.js modified

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var fs = require('fs');
+var out = fs.readFileSync('index.html').toString();
+app.get('/', function (request, response) {
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on " + port);
 });
